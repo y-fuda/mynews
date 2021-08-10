b86c389cd469a129eda2f4114237f724f2022f20
布田 <>
2021-08-10 23:24:26 +0000

    mynews commit

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+​
+class NewsController extends Controller
+{
+  public function add()
+  {
+      return view('admin.news.create');
+  }
+
+  // 以下を追記
+  public function create(Request $request)
+  {
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
+  }  
+}
